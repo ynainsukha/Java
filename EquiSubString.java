feccdb8b943e4e9e625afa28db644ce042783569
Yash Nainsukha <ynainsukha@hotmail.com>
2019-01-14 15:56:43 +0530

    Update and rename .java to EquiSubString.java

---
+++ EquiSubString.java
@@ -0,0 +1,28 @@
+package helper;
+
+/**
+ *
+ * @author Yash Nainsukha
+ */
+import java.util.LinkedList;
+
+public class JavaHelper {
+
+    /**
+     * @param string the input string
+     * @return number of sub-string if string can be divided into equal sub-strings 
+     */
+    public static int getEquiSubStringCount(String mString){
+        String sequence = "";
+        int string_count = 0;
+        for(int i = 0; i<mString.length()/2; i++) {
+            sequence = sequence + mString.charAt(i);
+            if (mString.matches("("+sequence+")*")) {
+                string_count = string.length()/sequence.length();
+                break;
+            }
+        }
+        return string_count;
+    }   
+}
+
