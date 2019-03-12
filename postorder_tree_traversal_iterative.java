bacf378ecc5f7d4baac7b54a57e155ef65dd4fd1
Yash Nainsukha <ynainsukha@hotmail.com>
2019-03-12 18:56:42 +0530

    Create postorder_tree_traversal_iterative.java

---
+++ postorder_tree_traversal_iterative.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> postOrder = new ArrayList<>();
+        if(root == null){ return postOrder;}
+        Stack<TreeNode> nodes = new Stack<>();
+        ArrayList<TreeNode> visited = new ArrayList<>();
+        nodes.push(root);
+        TreeNode temp;
+        while(!nodes.empty()){
+            temp = nodes.peek();
+            if(temp.left != null && visited.indexOf(temp.left) == -1){
+                nodes.push(temp.left);
+            }else if(temp.right != null && visited.indexOf(temp.right) == -1){
+                nodes.push(temp.right);
+            }else{
+                temp = nodes.pop();
+                postOrder.add(temp.val);
+                visited.add(temp);
+            }
+        }
+        return postOrder;
+    }
+}
