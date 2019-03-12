c0f7fe33340f26d2fb32a9a197a234a5ed3f5380
Yash Nainsukha <ynainsukha@hotmail.com>
2019-03-12 18:44:21 +0530

    Create Inorder_tree_traversal _iterative.java

---
+++ Inorder_tree_traversal _iterative.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> inOrder = new ArrayList<>();
+        if(root == null){
+            return inOrder;
+        }
+        Stack<TreeNode> nodes = new Stack<>();
+        nodes.push(root);
+        ArrayList<TreeNode> visited = new ArrayList<>(); 
+        TreeNode temp;
+        while(!nodes.empty()){
+            temp = nodes.peek();
+                if(temp.left != null && visited.indexOf(temp.left) == -1){
+                    nodes.push(temp.left);
+                }else{
+                    temp = nodes.pop();
+                    visited.add(temp);
+                    inOrder.add(temp.val);
+                    if(temp.right != null) { nodes.push(temp.right); }
+                }
+        }
+        return inOrder;
+    }
+}
